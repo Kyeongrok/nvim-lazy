eb07cf898b2689fab63fe52232bfe769426ef81f
Kyeongrok Kim <oceanfog1@gmail.com>
2025-03-11 14:58:35 +0900

    Update init.lua

--- init.lua
+++ init.lua
@@ -11,4 +11,17 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Leader
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
+
+-- Spaces & Tabs     
+vim.opt.tabstop = 4       -- number of visual spaces per TAB    
+vim.opt.softtabstop = 4   -- number of spaces in tab when editing    
+vim.opt.shiftwidth = 4    -- number of spaces to use for autoindent      
+vim.opt.expandtab = true  -- tabs are space    
+vim.opt.autoindent = true                                                                                                               
+vim.opt.copyindent = true -- copy indent from the previous line                               
+vim.opt.termguicolors = true
+
 require("lazy").setup("plugins")
